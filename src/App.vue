28d61df79c98660f72a6f139b3142a306f2f1851
xkrfer <xkrfer@gmail.com>
2022-03-29 22:36:57 +0800

    🎨 github_client_id从cookie中获取

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <span class="bg-[#eb6c31] px-[12px] py-[6px] rounded-[8px] text-white">{{ origin }}</span>
     </p>
     <img src="/clip.png" alt="clip" class="mx-auto">
-    <div class="pt-[48px]">
+    <div class="pt-[48px]" v-if="client_id">
       <span class="bg-[#3894ff] px-[12px] py-[6px] rounded-[8px] text-white cursor-pointer" @click="OAuthToGithub">Github登陆</span>
     </div>
   </div>
@@ -13,13 +13,14 @@
 
 
 <script setup lang="ts">
-
+// @ts-ignore
+import Cookies from 'js-cookie'
 const { origin } = window.location
-
-const client_id = '8c6833674f0e10827f8e'
-const redirect_uri= 'http://localhost:8800/oauth/redirect/github'
+const client_id = Cookies.get('github_client_id')
 const OAuthToGithub = () => {
-  window.location.href = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}`
+  if(client_id){
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${origin}/oauth/redirect/github`
+  }
 }
 
 </script>
