a7395722e289d0011751b98a49a9137f80c346cd
xkrfer <xkrfer@gmail.com>
2022-04-17 12:24:21 +0800

    🔥 去除github登录

--- src/App.vue
+++ src/App.vue
@@ -5,32 +5,14 @@
       <span class="bg-[#eb6c31] px-[12px] py-[6px] rounded-[8px] text-white">{{ origin }}</span>
     </p>
     <img src="/clip.png" alt="clip" class="mx-auto">
-    <div class="pt-[48px]" v-if="github_client_id">
-      <span class="bg-[#3894ff] px-[12px] py-[6px] rounded-[8px] text-white cursor-pointer" @click="OAuthToGithub">Github登陆</span>
-    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 // @ts-ignore
 import Cookies from 'js-cookie'
-import {onMounted, ref} from "vue";
-
+import {onMounted} from "vue";
 const {origin} = window.location
-const github_client_id = ref()
-const OAuthToGithub = () => {
-  if (github_client_id.value) {
-    window.location.href = `https://github.com/login/oauth/authorize?client_id=${github_client_id.value}&redirect_uri=${origin}/login/github`
-  }
-}
-
-const fetchAppId = async () => {
-  const res = await fetch("/appid")
-  const data = await res.clone().json()
-  if (data.code === 0) {
-    github_client_id.value = data.content.github
-  }
-}
 
 const getCookie = () => {
   const token = Cookies.get("token")
@@ -40,12 +22,10 @@ const getCookie = () => {
     }
   })
   document?.dispatchEvent(customEvent)
-  // Cookies.remove('token')
 }
 
 
 onMounted(() => {
-  fetchAppId()
   getCookie()
 })
 
